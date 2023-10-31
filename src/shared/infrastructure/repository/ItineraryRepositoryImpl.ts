7399a972f45f4c50bd10fee67df1fbc424c6985e
Luis Enrique Ramírez López <luian.ramirez.12@gmail.com>
2023-10-30 19:11:30 -0600

    fixes

--- src/shared/infrastructure/repository/ItineraryRepositoryImpl.ts
+++ src/shared/infrastructure/repository/ItineraryRepositoryImpl.ts
@@ -7,10 +7,16 @@ import firestore from '@react-native-firebase/firestore';
 
 export default class ItineraryRepositoryImpl implements ItineraryRepository {
   public async getItinerary(type: TypeItenaryEnum): Promise<IItinerary[]> {
-    const data = await firestore().collection('itinerary').doc(type).get();
+    const data = await firestore().collection('myApp').doc('itinerary').get();
 
     if (data.exists) {
-      return data.data() as IItinerary[];
+      const arrs = data.data();
+      if (arrs) {
+        const arr = arrs[type];
+        if (arr) {
+          return arr;
+        }
+      }
     }
 
     return [];
