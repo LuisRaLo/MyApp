583d50e23f2f07ea46f73e80b3c760cec1fd54d0
Luis Enrique Ramírez López <luian.ramirez.12@gmail.com>
2023-11-21 19:53:58 -0600

    change names andpackages

--- android/app/src/main/java/cl/andresm/jardintrip/MainActivity.java
+++ android/app/src/main/java/cl/andresm/jardintrip/MainActivity.java
@@ -1,4 +1,4 @@
-package com.myapp;
+package cl.andresm.jardintrip;
 
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
@@ -18,7 +18,7 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "myapp";
+    return "cl.andresm.jardintrip";
   }
 
   /**
